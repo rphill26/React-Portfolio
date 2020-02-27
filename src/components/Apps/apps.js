77b263963a69ef4cc09027c888a16391fe24d32a
Randall Phillips <rphill45@uncc.edu>
2020-02-27 13:26:45 -0500

    fixing routing

---
+++ src/components/Apps/apps.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Apps extends Component {
+  render() {
+    return (
+      <div>
+        <h2>My Apps & Projects</h2>
+        <p>These are my Apps & Projects</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/Resume/resume.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Resume</h2>
+        <p>This is my Resume</p>
+      </div>
+    );
+  }
+}

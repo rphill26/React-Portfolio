ce7c5eab4fddb274a2ca3bdb8ca9132babd03751
Randall Phillips <rphill45@uncc.edu>
2020-02-27 13:36:06 -0500

    Updates

---
+++ precache-manifest.cc3d51e711a5ee243e3d5d88f469fa1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aca8decf2351247c67ed81ff4a1d559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918723877fb2a631c03b",
+    "url": "/static/css/main.23c848c7.chunk.css"
+  },
+  {
+    "revision": "6489f9640306532d1a5d",
+    "url": "/static/js/2.e24351a4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.e24351a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918723877fb2a631c03b",
+    "url": "/static/js/main.d7deb7cf.chunk.js"
+  },
+  {
+    "revision": "f8269cb1c31ddeefa0ff",
+    "url": "/static/js/runtime-main.2e7b71eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.249cbf5a419ad6f78a01dc5e74bf83b7.js"
+  "/precache-manifest.cc3d51e711a5ee243e3d5d88f469fa1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
